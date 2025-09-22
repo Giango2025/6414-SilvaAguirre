4b286b9f6bc0b110004fd863d7dd993fc4f6e1e7
Gianfranco <n00468999@upn.pe>
2025-09-22 02:26:39 -0500

    Agregando condicional doble y arreglando carpetas de condicional

---
+++ EstructurasControl/Condicionales/Doble/doble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Llueve actualmente? (si/no)");
+            string respuesta = Console.ReadLine();
+
+            // Condicional doble simple y claro
+            if (respuesta.ToLower() == "si")
+            {
+                Console.WriteLine("☔ Lleva paraguas contigo");
+            }
+            else
+            {
+                Console.WriteLine("☀️ No necesitas paraguas hoy");
+            }
+
+            Console.WriteLine("¡Que tengas un buen día!");
+        }
+    }
+}
+
+
